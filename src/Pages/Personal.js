7e8f211de93b1a584c4517c735a7985a769e688b
Jerry Li <lmyjerry@gmail.com>
2022-02-26 12:52:03 -0500

    Day2 Morning

--- src/Pages/Personal.js
+++ src/Pages/Personal.js
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import {Box} from '@mui/system';
 import {Grid} from '@mui/material';
 import Table from '@mui/material/Table';
@@ -15,64 +12,17 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-function stringToColor(string) {
-    let hash = 0;
-    let i;
-  
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-  
-    let color = '#';
-  
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.substr(-2);
-    }
-    /* eslint-enable no-bitwise */
-  
-    return color;
-}
-  
-  function stringAvatar(name) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-    };
-}
-  
-function BackgroundLetterAvatars() {
-    return (
-      <Stack direction="row" spacing={2}>
-        <Avatar {...stringAvatar('Jerry Li')} />
-        <Avatar {...stringAvatar('Jack Barnett')} />
-      </Stack>
-    );
-}
+import { ResponsiveLine } from '@nivo/line'
 
-function ContainedButtons() {
-    return (
-      <Stack direction="row" spacing={2}>
-        <Button variant="contained">Contained</Button>
-        <Button variant="contained" disabled>
-          Disabled
-        </Button>
-        <Button variant="contained" href="#contained-buttons">
-          Link
-        </Button>
-      </Stack>
-    );
-}
+
 function createData(Date, Status, Amount) {
   return { Date, Status, Amount };
 }
 const rows = [
   createData('2022.02.24', "Pending", 101.00),
   createData('2022.02.23', "Completed", 1000.00),
-    createData('2022.02.22', "Completed", 9.99),
+  createData('2022.02.22', "Completed", 9.99),
+  createData('2022.02.22', "Completed", 9.98),
 ];
 
 const BasicTable = () => {
@@ -117,13 +67,13 @@ const BasicCard = () => {
   return (
     <Card sx={{ Width: "100%" }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="Green" gutterBottom>
+        <Typography sx={{ fontSize: 30 }} color="Green" gutterBottom>
           Current Deposits
         </Typography>
-        <Typography sx={{ fontSize: 30 }} color="Black" gutterBottom>
+        <Typography sx={{ fontSize: 40 }} color="Black" gutterBottom>
           $3,024.00
         </Typography>
-        <Typography sx={{ fontSize: 14 }} color="Black" gutterBottom>
+        <Typography sx={{ fontSize: 20 }} color="Black" gutterBottom>
           On 2022.02.25
         </Typography>
           
@@ -131,32 +81,131 @@ const BasicCard = () => {
     </Card>
   );
 }
+
+
+const data = [
+  {
+    "id": "User",
+    "color": "hsl(140, 100%, 0%)",
+    "data": [
+      {
+        "x": "Monday",
+        "y": 12
+      },
+      {
+        "x": "Tuesday",
+        "y": 171
+      },
+      {
+        "x": "Wednesday",
+        "y": 232
+      },
+      {
+        "x": "Thursday",
+        "y": 107
+      },
+      {
+        "x": "Friday",
+        "y": 157
+      },
+      {
+        "x": "Saturday",
+        "y": 205
+      },
+      {
+        "x": "Sunday",
+        "y": 229
+      }
+    ]
+  }
+]
+const MyResponsiveLine = ({ data }) => (
+  <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false
+      }}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'transportation',
+          legendOffset: 36,
+          legendPosition: 'middle'
+      }}
+      axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle'
+      }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={[
+          {
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 153, 51, 0.5)',
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemBackground: 'rgba(0, 0, 0, .03)',
+                          itemOpacity: 1
+                      }
+                  }
+              ]
+          }
+      ]}
+  />
+)
 export default function Personal() {
     return (
-    //BackgroundLetterAvatars(),
-    //ContainedButtons()
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Box boxShadow={3} sx={{height: 200, background: "#FFF"}}>
-          {BasicTable()}
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Box boxShadow={3} sx={{height: 200, background: "#FFF"}}>
-          {BasicCard()}
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box boxShadow={3} sx={{height: 200, background: "#FFF"}}>
-          BackgroundLetterAvatars() 
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box boxShadow={3} sx={{height: 200, background: "#FFF"}}>
-            content here
-          </Box>
-        </Grid>
-      </Grid>
-    );
+    <Grid container spacing={2}>
+    <Grid item xs={16} md={9}>
+      <Box boxShadow={3} sx={{height: 350, background: "#FFF"}}>
+      {MyResponsiveLine({data})}
+      </Box>
+    </Grid>
+    <Grid item xs={6} md={3}>
+      <Box boxShadow={3} sx={{height: 350, background: "#FFF"}}>
+      {BasicCard()}
+      </Box>
+    </Grid>
+    <Grid item xs={30} md={12}>
+      <Box boxShadow={3} sx={{height: 270, background: "#FFF"}}>
+      {BasicTable()}
+      </Box>
+    </Grid>
+  </Grid>
+);
+    
 }  
 
