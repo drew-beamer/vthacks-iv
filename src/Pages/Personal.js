862e4a680dff73a9245b2e653298f5a391659e6b
Jerry Li <lmyjerry@gmail.com>
2022-02-26 22:48:09 -0500

    Jerry Update

--- src/Pages/Personal.js
+++ src/Pages/Personal.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Box} from '@mui/system';
-import {Grid} from '@mui/material';
+import { Box } from '@mui/system';
+import { Grid } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -12,19 +12,6 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-<<<<<<< Updated upstream
-import { ResponsiveLine } from '@nivo/line'
-<script data-main="lib/capital_one" src="lib/require-jquery.js"></script>
-function createData(Date, Status, Amount) {
-  return { Date, Status, Amount };
-}
-const rows = [
-  createData('2022.02.24', "Pending", 101.00),
-  createData('2022.02.23', "Completed", 1000.00),
-  createData('2022.02.22', "Completed", 9.99),
-  createData('2022.02.22', "Completed", 9.98),
-];
-=======
 import { ResponsiveLine } from '@nivo/line';
 import { useEffect } from 'react';
 
@@ -41,9 +28,8 @@ const rows = (deposits, withdrawals) => {
   const data_sum = data_depo.push(data_with)
   return data_sum.sort(sortDownDate)
 };
->>>>>>> Stashed changes
 
-const BasicTable = () => {
+const BasicTable = (rows) => {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ Width: "100%" }} aria-label="simple table">
@@ -61,7 +47,7 @@ const BasicTable = () => {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-              {row.Date}
+                {row.Date}
               </TableCell>
               <TableCell align="right">{row.Status}</TableCell>
               <TableCell align="right">{row.Amount}</TableCell>
@@ -81,7 +67,7 @@ const bull = (
   </Box>
 );
 
-const BasicCard = () => {
+const BasicCard = (balance,nickname) => {
   return (
     <Card sx={{ Width: "100%" }}>
       <CardContent>
@@ -89,13 +75,12 @@ const BasicCard = () => {
           Current Deposits
         </Typography>
         <Typography sx={{ fontSize: 40 }} color="Black" gutterBottom>
-          $3,024.00
+          ${balance}
         </Typography>
-        <Typography sx={{ fontSize: 20 }} color="Black" gutterBottom>
-        <br />
-        <br />
-        <br />
-          On 2022.02.25
+        <Typography sx={{ fontSize: 20 }} color="Green" gutterBottom>
+          <br />
+          <br />
+          ${nickname}
         </Typography>
 
       </CardContent>
@@ -103,10 +88,6 @@ const BasicCard = () => {
   );
 }
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 const data = [
   {
     "id": "User",
@@ -136,8 +117,6 @@ const data = [
         "x": "Saturday",
         "y": 205
       },
-<<<<<<< Updated upstream
-=======
       {
         "x": "Sunday",
         "y": 229
@@ -188,101 +167,34 @@ const MyResponsiveLine = ({ data }) => (
     pointLabelYOffset={-12}
     useMesh={true}
     legends={[
->>>>>>> Stashed changes
       {
-        "x": "Sunday",
-        "y": 229
-      }
-    ]
-  }
-]
-const MyResponsiveLine = ({ data }) => (
-  <ResponsiveLine
-      data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: 'point' }}
-      yScale={{
-          type: 'linear',
-          min: 'auto',
-          max: 'auto',
-          stacked: true,
-          reverse: false
-      }}
-      yFormat=" >-.2f"
-      curve="cardinal"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-          orient: 'bottom',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Date',
-          legendOffset: 36,
-          legendPosition: 'middle'
-      }}
-      axisLeft={{
-          orient: 'left',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Amount',
-          legendOffset: -40,
-          legendPosition: 'middle'
-      }}
-      pointSize={10}
-      pointColor={{ theme: 'background' }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: 'serieColor' }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 100,
+        translateY: 0,
+        itemsSpacing: 0,
+        itemDirection: 'left-to-right',
+        itemWidth: 80,
+        itemHeight: 20,
+        itemOpacity: 0.75,
+        symbolSize: 12,
+        symbolShape: 'circle',
+        symbolBorderColor: 'rgba(0, 153, 51, 0.5)',
+        effects: [
           {
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: 'left-to-right',
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: 'circle',
-              symbolBorderColor: 'rgba(0, 153, 51, 0.5)',
-              effects: [
-                  {
-                      on: 'hover',
-                      style: {
-                          itemBackground: 'rgba(0, 0, 0, .03)',
-                          itemOpacity: 1
-                      }
-                  }
-              ]
+            on: 'hover',
+            style: {
+              itemBackground: 'rgba(0, 0, 0, .03)',
+              itemOpacity: 1
+            }
           }
-      ]}
+        ]
+      }
+    ]}
   />
 )
 export default function Personal() {
-<<<<<<< Updated upstream
-    return (
-    <Grid container spacing={2}>
-    <Grid item xs={16} md={9}>
-      <Box boxShadow={3} sx={{height: 300, background: "#FFF"}}>
-      {MyResponsiveLine({data})}
-      </Box>
-    </Grid>
-    <Grid item xs={6} md={3}>
-      <Box boxShadow={3} sx={{height: 290, background: "#FFF"}}>
-      {BasicCard()}
-      </Box>
-    </Grid>
-    <Grid item xs={30} md={12}>
-      <Box boxShadow={3} sx={{height: 250, background: "#FFF"}}>
-      {BasicTable()}
-      </Box>
-=======
   const [deposits, setDeposits] = React.useState([])
   const [withdrawals, setWithdrawals] = React.useState([])
   const [accounts, setAccounts] = React.useState([])
@@ -346,13 +258,10 @@ export default function Personal() {
       </Grid>
       <Grid item xs={30} md={12}>
         <Box boxShadow={3} sx={{ height: 250, background: "#FFF" }}>
-          {deposits.length ? BasicTable(datarows) : "No data"}
+        {deposits.length ? BasicTable(datarows) : "No data"}
         </Box>
       </Grid>
->>>>>>> Stashed changes
     </Grid>
-  </Grid>
 
-);   
-}  
-
+  );
+}
